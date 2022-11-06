a6a6b4b0b7835c4b305f5a65e5d3de8e0623d65b
sawyerg3 <sawyerg333@gmail.com>
2022-11-06 01:09:16 -0600

    Connect to mongoDB

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const app = express();
-
+const connectDB = require("./config/db");
 const port = process.env.PORT;
+
+connectDB();
 
 app.get("/api", (req, res) => {
   res.json({ users: ["one", "next", "lastUser"] });
